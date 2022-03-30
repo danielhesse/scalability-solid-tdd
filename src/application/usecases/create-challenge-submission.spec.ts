dacf8c7dc691d6307368e3cdb030681278490026
Daniel Hessel <daniel@sharbe.com.br>
2022-03-29 21:53:39 -0300

    feat: finalized code

--- src/application/usecases/create-challenge-submission.spec.ts
+++ src/application/usecases/create-challenge-submission.spec.ts
@@ -1,11 +1,26 @@
 import { InMemoryChallengesRepository } from "../../../tests/repositories/in-memory-challenges-repository";
 import { InMemoryStudentsRepository } from "../../../tests/repositories/in-memory-students-repository";
+import { Challenge } from "../../domain/entities/challenge";
+import { Student } from "../../domain/entities/student";
 import { CreateChallengeSubmission } from "./create-challenge-submission";
 
 describe("Create challenge submission use case", () => {
   it("should be able to create a new challenge submission", () => {
     const studentsRepository = new InMemoryStudentsRepository();
     const challengesRepository = new InMemoryChallengesRepository();
+
+    const student = Student.create({
+      name: "Daniel Hessel",
+      email: "daniel@sharbe.com.br"
+    });
+
+    const challenge = Challenge.create({
+      title: "SOLID + TDD",
+      instructionUrl: "http://example.com"
+    });
+
+    studentsRepository.repository.push(student);
+    challengesRepository.repository.push(challenge);
 
     // sut = system under test (by Rodrigo Manguinho)
     const sut = new CreateChallengeSubmission(
@@ -14,8 +29,8 @@ describe("Create challenge submission use case", () => {
     );
 
     const response = sut.execute({
-      studentId: "fake-student-id",
-      challengeId: "fake-challenge-id",
+      studentId: student.id,
+      challengeId: challenge.id,
     });
 
     expect(response).toBeTruthy();
